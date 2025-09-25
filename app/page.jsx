1573a880dfa94235f7362633b0df4fab8108453d
ComplianceGGPH <compliance@ggpholdings.com>
2025-09-25 12:07:57 +0800

    forgot to add routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome</h1>
       <p>This is my Next.js app</p>
+      <Link
+        href={`/kanban`}
+      >
+        Go to Kanban Board
+      </Link>
     </div>
   );
 }
